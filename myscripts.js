225f795125338c8a8670218b652bb77c17ba223c
NicholasDo18 <146508658+NicholasDo18@users.noreply.github.com.>
2025-03-26 19:49:58 -0400

    Add function to play rock paper scissors. Took the human and computer choices as arguments and used else if statements

--- myscripts.js
+++ myscripts.js
@@ -23,3 +23,48 @@ function getHumanChoice(){
     let option = prompt("Rock, Paper, or Scissors");
     return option.toLocaleLowerCase();
 }
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "rock" && computerChoice === "rock"){
+        console.log("Draw.")
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper"){
+        console.log("You lose! Paper beats Rock.");
+        computerScore += 1;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win! Rock beats Paper.");
+        humanScore += 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "paper"){
+        console.log("Draw.");
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You lose! Scissors beats Paper.");
+        computerScore += 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors"){
+        console.log("You win! Paper beats Rock.");
+        humanScore += 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "scissors"){
+        console.log("Draw.");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock"){
+        console.log("You lose! Rock beats Scissors.");
+        computerScore += 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win! Scissors beats Paper.");
+        humanScore += 1;
+    }
+    else{
+        console.log("INVALID OPTION. please try again.");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
